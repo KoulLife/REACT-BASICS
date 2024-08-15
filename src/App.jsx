9a2a5c5027e16846243f0a4872cfff1aa8bbd67b
Koul <dongik0122@gmail.com>
2024-08-15 19:09:34 +0900

    feat : event handler
    핸들러와 이벤트 핵습

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header(){
-  const randomWord = reactDescription[getRandomInt(2)];
-
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {randomWord} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import {CORE_CONCEPTS} from "./data"
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+
+  function handleSelect(selectedButton){
+    console.log(selectedButton)
+  }
+
   return (
     <div>
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>CORE Concepts</h2>
+          <ul>
+            <li><CoreConcept {...CORE_CONCEPTS[0]}/></li>
+            <li><CoreConcept {...CORE_CONCEPTS[1]}/></li>
+            <li><CoreConcept {...CORE_CONCEPTS[2]}/></li>
+            <li><CoreConcept {...CORE_CONCEPTS[3]}/></li>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect = {() => handleSelect('Components')}>Components</TabButton>
+            <TabButton>Components</TabButton>
+            <TabButton>Components</TabButton>
+            <TabButton>Components</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
